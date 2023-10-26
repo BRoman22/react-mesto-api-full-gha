ca7ba62fc93ad396f944af883c38c7af223b45d9
Roman Bondarev <Rbondarev22@gmail.com>
2023-10-26 20:21:15 +0700

    test gha

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ app.use(
     origin: [
       'https://mesto.nomoredomainsrocks.ru',
       'http://mesto.nomoredomainsrocks.ru',
+      'http://localhost:3000',
       // 'http://localhost:5173',
     ],
     credentials: true,
